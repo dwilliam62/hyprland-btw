ea30da0b6d500c88bf919f629d93a1aebd1f9660
Don Williams <Don.e.williams@gmail.com>
2026-09-19 17:27:52 -0400

    Removed neovim overlay added 12.5
    Signed-off-by: Don Williams <Don.e.williams@gmail.com>
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   overlays.nix

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,11 +1,6 @@
 {inputs, ...}: {
   nixpkgs.overlays = [
     (final: prev: {
-      # Pin Neovim to nixpkgs-stable (0.11.x) to avoid regressions
-      neovim = inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system}.neovim;
-      neovim-unwrapped =
-        inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system}.neovim-unwrapped;
-
       synfetch = inputs.synfetch.packages.${final.stdenv.hostPlatform.system}.default;
     })
   ];
